3adf2d1c1ca02a81f28dd4f745f2cdde05dcb89a
Joel-pixel006 <joeljo2146@gmail.com>
2025-04-14 23:07:02 +0530

    Update qrscanner.py

--- qrscanner.py
+++ qrscanner.py
@@ -1,6 +1,6 @@
 import qrcode
 
-
+#this ststemnts below shows using make class and converting it into png format using pillow package
 
 myqr = qrcode.make("https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile")
 #converting it into image using pillow
